e88ec15ebc0c99910aabb04f3cf6c92cb93d8507
徐远翔@我心爱的Thinkpad <xuyuanxiang@wosai-inc.com>
2018-07-11 17:24:43 +0800

    done

--- pages/start-activity.js
+++ pages/start-activity.js
@@ -1,17 +1,18 @@
 import { preCreate } from '../services/order-service.js';
 
-customElements.define('start-activity', class extends HTMLElement {
+window.customElements.define('start-activity', class extends HTMLElement {
   constructor () {
     super();
-    this.state = {
-      text: 'Hello',
-    };
     this.attachShadow({mode: 'open'});
-    this.innerHTML = `
-      <h1>${this.state.text}</h1>
-      <button id="btn"></button>
+    const style = document.createElement('style');
+    style.textContent = `
+    :host {display: block; height:100%;} 
+    button {width: 100px;height:44px;line-height:44px;background: red;}
     `;
-    this.querySelector('#btn').
-      addEventListener('click', () => preCreate(), false);
+    this.appendChild(style);
+    const btn = document.createElement('button');
+    btn.addEventListener('click', () => preCreate(), false);
+    btn.textContent = 'Click Me';
+    this.appendChild(btn);
   }
 });
